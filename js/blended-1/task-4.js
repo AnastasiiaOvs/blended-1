3d7310ddee268432579d0b13309712923a3e92b1
AnastasiiaOvs <a.alexandrovna@i.ua>
2025-03-22 17:39:43 +0200

    blended

---
+++ js/blended-1/task-4.js
@@ -0,0 +1,8 @@
+// Використайте цикл while, щоб вивести в console всі числа від 0 до 20 включно.
+
+// let number = 0;
+
+// while (number <= 20) {
+//     console.log(number);
+//     number += 1;
+// }
